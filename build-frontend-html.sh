5aeccb91491ec56f595af3de3b1aa690e0458bd2
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2023-09-04 18:33:45 -0300

    Restructure project to prevent cabal hell.
    Html exec put to frontend-html package.
    All other is put to frontend package.
    Script files updated.

--- build-frontend-html.sh
+++ build-frontend-html.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-printf '\n==== Build Frontend html ====\n\n'
-cabal new-run --project-file=frontend-html.project frontend-html
-
-printf '\n==== Build Frontend ====\n\n'
-cabal new-build --ghcjs --project-file=frontend.project frontend
-
-printf '\n==== Copy js files to result ====\n\n'
-
+cabal run --project-file=frontend-html.project frontend-html
 cp dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/encoins-frontend-0.1.0.0/x/encoins-website/build/encoins-website/encoins-website.jsexe/all.js result/index.js
 cp dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/encoins-frontend-0.1.0.0/x/encoins-app/build/encoins-app/encoins-app.jsexe/all.js result/app.js
 cp dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/encoins-frontend-0.1.0.0/x/encoins-dao/build/encoins-dao/encoins-dao.jsexe/all.js result/dao.js
