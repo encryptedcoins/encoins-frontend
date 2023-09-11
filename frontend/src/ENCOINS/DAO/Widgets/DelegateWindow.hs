cc70f9ffce0b32550bab6dfef21b14aaa5a5f25a
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2023-09-11 15:05:56 -0300

    do not check empty input. Refactor.

--- frontend/src/ENCOINS/DAO/Widgets/DelegateWindow.hs
+++ frontend/src/ENCOINS/DAO/Widgets/DelegateWindow.hs
@@ -7,104 +7,106 @@ module ENCOINS.DAO.Widgets.DelegateWindow
 import           Control.Monad                          (void)
 import           Reflex.Dom
 
-import           Backend.Protocol.Types
 import           ENCOINS.App.Widgets.Basic              (elementResultJS, containerApp)
 import           ENCOINS.Common.Widgets.Advanced        (dialogWindow)
-import           ENCOINS.Common.Events (addFocusPostBuildDelayE, logEvent, newEventWithDelay)
+import           ENCOINS.Common.Events (addFocusPostBuildDelayE, logEvent)
 import           Backend.Wallet (Wallet(..), toJS)
 import qualified JS.DAO as JS
-import           Data.Text (Text, unpack)
+import           Data.Text (Text)
 import           ENCOINS.Common.Widgets.Basic (btn, divClassId)
 import           ENCOINS.Common.Utils (toText)
 import           Backend.Status (Status(..), otherError)
-import Data.Bool (bool)
-import Control.Monad.IO.Class (MonadIO (liftIO))
-import Debug.Trace (trace)
+import           Data.Monoid (Any(..))
+import           Data.Bool (bool)
+import qualified Data.Text as T
 
 delegateWindow :: MonadWidget t m
   => Event t ()
   -> Dynamic t Wallet
   -> m (Event t Text, Event t ())
 delegateWindow eOpen dWallet = mdo
-  (eOk, eEscape) <- dialogWindow True eOpen (leftmost [void eOk, eEscape]) "width: 950px; padding-left: 70px; padding-right: 70px; padding-top: 30px; padding-bottom: 30px" "" $ mdo
-      divClass "connect-title-div" $ divClass "app-text-semibold" $ text "Enter url:"
+  (eUrlOk, eEscape) <- dialogWindow
+    True
+    eOpen
+    (leftmost [void eUrlOk, eEscape])
+    "width: 950px; padding-left: 70px; padding-right: 70px; padding-top: 30px; padding-bottom: 30px"
+    "" $ mdo
+          divClass "connect-title-div" $ divClass "app-text-semibold" $ text "Enter url:"
 
-      (dUrl, eEscape) <- inputWidget eOpen (checkStatus <$> dStatus)
-      performEvent_
-        (JS.checkUrl <$> updated (traceDynWith (\u -> "dUrl: " <> unpack u) dUrl))
+          (dInputText, eEscape) <- inputWidget eOpen (isDisableStatus <$> dStatus)
+          let eInputText = updated dInputText
 
-      eValidUrl <- updated <$> elementResultJS "ValidUrl" id
-      eInvalidUrl <- updated <$> elementResultJS "InvalidUrl" id
-      logEvent "eValidUrl" eValidUrl
-      logEvent "eInvalidUrl" eInvalidUrl
+          let eNonEmptyUrl = ffilter (not . T.null) eInputText
+          let eEmptyUrl = ffilter T.null eInputText
 
-      let eCheckedUrl = leftmost
-            [
-              Nothing  <$ eValidUrl
-            , Just ()  <$ eInvalidUrl
-            ]
-      logEvent "eCheckedUrl" eCheckedUrl
+          -- Check url when it is ONLY not empty
+          performEvent_ (JS.checkUrl <$> eNonEmptyUrl)
 
-      dCheckedUrl <- holdDyn (Just ()) eCheckedUrl
+          eValidUrl <- updated <$> elementResultJS "ValidUrl" id
+          eInvalidUrl <- updated <$> elementResultJS "InvalidUrl" id
 
-      let dCheckedStatus = checkStatus <$> dStatus
+          let eIsInvalidUrl = leftmost
+                [
+                  True  <$ eEmptyUrl
+                , True  <$ eInvalidUrl
+                , False <$ eValidUrl
+                ]
 
-      btnOk <- buttonWidget $ mconcat
-        [ traceDynWith (\cl -> "dCheckedStatus: " <> show cl) dCheckedStatus
-        , traceDynWith (\cl -> "dCheckedUrl: " <> show cl) dCheckedUrl
-        ]
+          dIsInvalidUrl <- holdDyn True eIsInvalidUrl
 
-      logEvent "btnOk" btnOk
+          let dIsDisableStatus = isDisableStatus <$> dStatus
 
-      let eUrl = traceEvent "eUrl" $ tagPromptlyDyn dUrl btnOk
+          let eUrl = tagPromptlyDyn dInputText btnOk
 
-      performEvent_
-        $ JS.daoDelegateTx <$> attachPromptlyDyn (fmap (toJS . walletName) dWallet) eUrl
+          performEvent_
+            $ JS.daoDelegateTx <$> attachPromptlyDyn (fmap (toJS . walletName) dWallet) eUrl
 
-      isDelegate <- elementResultJS "Delegate" id
-      logEvent "Delegate" $ checkEmptyText <$> updated isDelegate
+          eStatus <- delegateStatus eInvalidUrl eValidUrl eEmptyUrl
+          logEvent "eStatus" $ ffilter (not . null) $ show <$> eStatus
+          dStatus <- holdDyn Ready eStatus
 
-      eStatus <- delegateStatus eInvalidUrl eValidUrl
-      dStatus <- holdDyn Ready eStatus
-      containerApp ""
-        $ divClassId "app-text-small" "welcome-read-docs"
-        $ dynText
-        $ toText <$> traceDynWith (\dSt -> "dStatus: " <> show dSt) dStatus
+          -- The button disable with invalid url and performant statuses.
+          btnOk <- divClass "app-top-menu-div" $ mdo
+            btnOk <- divClass "menu-item-button-right" $ do
+              buttonWidget $ fmap getAny $ mconcat $ fmap (fmap Any)
+                [ dIsDisableStatus, dIsInvalidUrl]
+            divClass "menu-item-button-right" $ do
+              containerApp ""
+                $ divClassId "app-text-small" "delegation-status"
+                $ dynText
+                $ toText <$> dStatus
+            pure btnOk
 
-      logEvent "eStatus" eStatus
-
-      return (eUrl, eEscape)
-  return (eOk, eEscape)
+          return (eUrl, eEscape)
+  return (eUrlOk, eEscape)
 
 delegateStatus :: MonadWidget t m
   => Event t Text
   -> Event t Text
+  -> Event t Text
   -> m (Event t Status)
-delegateStatus eInvalidUrl eValidUrl = do
+delegateStatus eInvalidUrl eValidUrl eEmptyUrl = do
   eConstruct <- updated <$> elementResultJS "DelegateCreateNewTx" id
   eSign <- updated <$> elementResultJS "DelegateSignTx" id
   eSubmit <- updated <$> elementResultJS "DelegateSubmitTx" id
   eErr <- updated <$> elementResultJS "DelegateError" id
   eErrStatus <- otherError eErr
-  -- eCustom <- newEventWithDelay 5
-  -- eCustomErr <- newEventWithDelay 15
   pure $ leftmost [
           eErrStatus
-        , Constructing             <$ eConstruct
-        , Signing                  <$ eSign
-        , Submitting               <$ eSubmit
-        -- , Signing <$ eCustom
-        -- , OtherError "custom Error" <$ eCustomErr
-        , Ready                    <$ eValidUrl
-        , OtherError "Invalid url" <$ eInvalidUrl
+        , Constructing                     <$ eConstruct
+        , Signing                          <$ eSign
+        , Submitting                       <$ eSubmit
+        , Ready                            <$ eValidUrl
+        , CustomStatus "Invalid reley url" <$ eInvalidUrl
+        , CustomStatus ""                  <$ eEmptyUrl
         ]
 
 inputWidget :: MonadWidget t m
   => Event t ()
-  -> Dynamic t (Maybe ())
+  -> Dynamic t Bool
   -> m (Dynamic t Text, Event t ())
-inputWidget eOpen dStatus = divClass "app-columns w-row" $ do
-    let dAttrs = maybe mempty (const $ "disabled" =: "") <$> dStatus
+inputWidget eOpen dDisableStatus = divClass "w-row" $ do
+    let dAttrs = bool mempty ("disabled" =: "") <$> dDisableStatus
     modifyAttrs <- dynamicAttributesToModifyAttributes dAttrs
     inp <- inputElement $ def
       & initialAttributes .~
@@ -120,24 +122,18 @@ inputWidget eOpen dStatus = divClass "app-columns w-row" $ do
     return (value inp, keydown Escape inp)
 
 buttonWidget :: MonadWidget t m
-  => Dynamic t (Maybe ())
+  => Dynamic t Bool
   -> m (Event t ())
 buttonWidget dStatus = btn
-    (traceDynWith (\cl -> "btClass: " <> unpack cl) $ mkBtnAttrs dStatus)
+    (mkBtnAttrs dStatus)
     "width:30%;display:inline-block;margin-right:5px;"
     (text "Ok")
   where
     btnOnAttrs = "button-switching inverted flex-center"
     btnOffAttrs = "button-switching inverted flex-center button-disabled"
-    mkBtnAttrs dSt = maybe btnOnAttrs (const btnOffAttrs) <$> dSt
+    mkBtnAttrs dSt = bool btnOnAttrs btnOffAttrs <$> dSt
 
--- For checkUrl and checkStatus
--- Just () stays for constraint, for 'disabled' attribute
--- Nothing for mempty
--- By default used mempty.
--- Just () applied to disabled for able to use Monoid on Maybe.
-checkStatus :: Status -> Maybe ()
-checkStatus status =
-  if status `elem` [Constructing, Signing, Submitting]
-    then Just ()
-    else Nothing
+-- Check if status is the performant one.
+-- Performant status fires when background operations are processing.
+isDisableStatus :: Status -> Bool
+isDisableStatus status = status `elem` [Constructing, Signing, Submitting]
