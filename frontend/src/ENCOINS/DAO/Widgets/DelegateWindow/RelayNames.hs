8a73c1ba3d3e6c6a89ca7312d4a870fa57da68d1
Vladimir Sinyakov <encoins.crypto@ud.me>
2023-11-30 21:37:16 +0100

    Update relay names

--- frontend/src/ENCOINS/DAO/Widgets/DelegateWindow/RelayNames.hs
+++ frontend/src/ENCOINS/DAO/Widgets/DelegateWindow/RelayNames.hs
@@ -9,5 +9,18 @@ import           Data.Text                       (Text)
 relayNames :: Map Text Text
 relayNames = fromList
   [
-    ("http://156.232.53.23:3000/", "ENCOINS Test Relay")
+    ("http://156.232.53.23:3000/", "ENCOINS Test Relay"),
+    ("http://209.145.61.188:3000", "PetLoverStake"),
+    ("http://167.71.10.20", "Waterboiler"),
+    ("http://61.77.248.249", "KTOP SPO"),
+    ("http://128.199.195.27", "Waffle"),
+    ("http://136.243.152.100", "[CHIL] Chile Stake pool"),
+    ("http://216.152.65.248", "$conrad"),
+    ("http://146.190.63.130", "Peter Bui"),
+    ("http://5.189.164.41", "Markus"),
+    ("http://82.165.230.141", "OYSTR"),
+    ("http://146.190.34.49", "Kodjo AKPONDEOU"),
+    ("http://encoins-relay.cardanistas.io", "Cardanistas Stake Pool"),
+    ("http://encoins.reservoir.network:3000", "Tom - BURN pool"),
+    ("http://144.91.80.26", "Toby Relayer")
   ]
