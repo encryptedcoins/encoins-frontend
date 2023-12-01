78e47e8d6909f0fd0dfee8e68846616d12143486
Vladimir Sinyakov <encoins.crypto@ud.me>
2023-12-01 16:56:58 +0100

    Update testnet setup

--- frontend/src/Backend/Protocol/Setup.hs
+++ frontend/src/Backend/Protocol/Setup.hs
@@ -23,7 +23,7 @@ minAdaTxOutInLedger = 4_000_000
 encoinsCurrencySymbol :: Text
 encoinsCurrencySymbol = case app networkConfig of
   Mainnet -> "f8fb0501e8966286e9e188860c4a48ee856eff5ae23d988812b3fcc2"
-  Testnet -> "fafd6f6d1a319af7f4a9638345ce7c3992dc3ba42bf6c99008e4ecb3"
+  Testnet -> "fa765a4f65920d1aaa4a072457d27a00d81374245afbe33d94fc1671"
 
 ledgerAddress :: Address
 ledgerAddress = Address
@@ -42,7 +42,7 @@ ledgerAddress = Address
   where
     validatorHex = case app networkConfig of
       Mainnet -> "93ee21257ac2a126e3afb273cbf59635365d7f554751ab936bcaddf0"
-      Testnet -> "4eca152b92576fcd88c4621f194a356273236f957fdb287cfd0ee4a3"
+      Testnet -> "09cc78edf1b8cd8243613de8d3ee6d6c8752e60241fe02929b5e8685"
     pubKeyHex = case app networkConfig of
       Mainnet -> "0bd016f8ba5857d2e2026da550e4b724a3e1e8d5598cbfab19ce756c"
-      Testnet -> "3c2c08be107291be8d71bbb32da11f3b9761b0991f2a6f6940f4f390"
+      Testnet -> "0bd016f8ba5857d2e2026da550e4b724a3e1e8d5598cbfab19ce756c"
--- frontend/src/ENCOINS/DAO/Widgets/DelegateWindow/RelayNames.hs
+++ frontend/src/ENCOINS/DAO/Widgets/DelegateWindow/RelayNames.hs
@@ -14,7 +14,7 @@ relayNames = fromList
     ("http://61.77.248.249", "KTOP SPO"),
     ("http://128.199.195.27", "Waffle"),
     ("https://136.243.152.100", "[CHIL] Chile Stake pool"),
-    ("http://encoins.bladepool.com/", "$conrad"),
+    ("http://encoins.bladepool.com", "$conrad"),
     ("http://146.190.63.130", "Peter Bui"),
     ("https://5.189.164.41", "Southtyrol Pool"),
     ("http://82.165.230.141", "OYSTR"),
@@ -23,5 +23,6 @@ relayNames = fromList
     ("http://encoins.reservoir.network", "BURN pool"),
     ("http://144.91.80.26", "Toby Relayer"),
     ("http://167.71.10.20", "zencs-relay"),
-    ("http://38.242.237.17", "cardano.uno")
+    ("http://38.242.237.17", "cardano.uno"),
+    ("http://161.97.73.124", "SafeStride relay")
   ]
