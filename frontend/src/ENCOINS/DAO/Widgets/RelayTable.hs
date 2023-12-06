3760e6be97495a1b65b0583def69ec9842462ab8
Vladislav <>
2023-12-06 19:19:45 -0300

    Show info how many encoins and where wallet delegated

--- frontend/src/ENCOINS/DAO/Widgets/RelayTable.hs
+++ frontend/src/ENCOINS/DAO/Widgets/RelayTable.hs
@@ -6,9 +6,11 @@ module ENCOINS.DAO.Widgets.RelayTable
   (
     relayAmountWidget
   , fetchRelayTable
+  , fetchDelegatedByAddress
   ) where
 
 import           Control.Monad                                 (forM)
+import           Data.Bool                                     (bool)
 import           Data.List                                     (sortOn)
 import qualified Data.Map                                      as Map
 import           Data.Maybe                                    (fromMaybe)
@@ -16,6 +18,7 @@ import           Data.Ord                                      (Down (..))
 import           Data.Text                                     (Text)
 import           Reflex.Dom
 
+import           Backend.Protocol.Types
 import           Backend.Servant.Requests                      (serversRequestWrapper)
 import           Backend.Utility                               (switchHoldDyn)
 import           Config.Config                                 (delegateServerUrl)
@@ -26,9 +29,11 @@ import           ENCOINS.DAO.Widgets.DelegateWindow.RelayNames (relayNames)
 
 relayAmountWidget :: MonadWidget t m
   => Event t (Either Int [(Text, Integer)])
+  -> Event t (Maybe (Text, Integer))
   -> m (Event t Text)
-relayAmountWidget eeRelays = do
+relayAmountWidget eeRelays emDelegated = do
   deRelays <- holdDyn (Right []) eeRelays
+  dmDelegated <- holdDyn Nothing emDelegated
   switchHoldDyn deRelays $ \case
     Left err -> do
       article $
@@ -36,13 +41,12 @@ relayAmountWidget eeRelays = do
       pure never
     Right relays -> article $ table $ do
       el "thead" $ tr $
-        mapM_ (\h -> th $ text h) ["Relay", "Delegated", ""]
+        mapM_ (\h -> th $ text h) ["Relay", "Total", "Delegated", ""]
       el "tbody" $ do
         evs <- forM relays $ \(relay, amount) -> tr $ do
           tdRelay $ text $ fromMaybe relay (relay `Map.lookup` relayNames)
-          tdAmount
-            $ text
-            $ toText (floor @Double $ fromIntegral amount / 1000000 :: Integer) <> " ENCS"
+          tdAmount $ text $ mkAmount amount
+          tdAmount $ dynText $ mkDelegated relay <$> dmDelegated
           eClick <- tdButton $ btn "button-switching inverted" "" $ text "Delegate"
           pure $ relay <$ eClick
         pure $ leftmost evs
@@ -54,6 +58,29 @@ relayAmountWidget eeRelays = do
     tdRelay = elAttr "td" ("class" =: "dao-DelegateWindow_TableRelay")
     tdAmount = elAttr "td" ("class" =: "dao-DelegateWindow_TableAmount")
     tdButton = elAttr "td" ("class" =: "dao-DelegateWindow_TableButton")
+
+fetchRelayTable :: MonadWidget t m
+  => Event t ()
+  -> m (Event t (Either Int [(Text, Integer)]))
+fetchRelayTable eOpen = do
+  eServers <- serversRequestWrapper delegateServerUrl eOpen
+  let eeRes = fmap (sortOn (Down . snd) . Map.toList) <$> eServers
+  pure eeRes
+
+fetchDelegatedByAddress :: MonadWidget t m
+  => Dynamic t Address
+  -> Event t ()
+  -> m (Event t (Maybe (Text, Integer)))
+fetchDelegatedByAddress dAddr eFire = do
+  pure $ Just ("https://encoins.io", 1000000) <$ eFire
+
+mkAmount :: Integer -> Text
+mkAmount amount =
+  toText (floor @Double $ fromIntegral amount / 1000000 :: Integer) <> " ENCS"
+
+mkDelegated :: Text -> Maybe (Text, Integer) -> Text
+mkDelegated relay =
+  maybe "" (\(r,n) -> bool "" (mkAmount n) (r == relay))
 
 -- sortRelayAmounts :: Maybe (Map Text String) -> [(Text, Integer)]
 -- sortRelayAmounts =
@@ -68,11 +95,3 @@ relayAmountWidget eeRelays = do
 -- fetchRelayTable eOpen = do
 --   let eUrl = "https://encoins.io/delegations.json" <$ eOpen
 --   fmap sortRelayAmounts <$> getAndDecode eUrl
-
-fetchRelayTable :: MonadWidget t m
-  => Event t ()
-  -> m (Event t (Either Int [(Text, Integer)]))
-fetchRelayTable eOpen = do
-  eServers <- serversRequestWrapper delegateServerUrl eOpen
-  let eeRes = fmap (sortOn (Down . snd) . Map.toList) <$> eServers
-  pure eeRes
