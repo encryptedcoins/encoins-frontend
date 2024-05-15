bd720e5a9266f128f61b8cdfacd1a258c816d278
Vladislav <>
2024-05-15 14:56:17 -0300

    Fix relay down bug in ledger on mainnet.
    Now Init fires when dUrls is updated

--- run_dev.sh
+++ run_dev.sh
@@ -9,7 +9,7 @@ printf '\n\n==== Build frontend for development ====\n'
 if ! build_dev; then
   printf "\n\nBuilding is failed."
 else
-  printf "Building is successful!"
+  printf "Dev is built successfully!"
   if copy_js "$version"; then
     if ! caddy run; then
       printf "\n\nCaddy running is failed."
