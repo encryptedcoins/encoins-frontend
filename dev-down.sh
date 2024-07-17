e8d8292dd5256c7684a8796ebe14bbc2c09b7b0a
Vladislav Sabanov <email@email.com>
2024-07-18 02:05:36 +0500

    WIP

--- dev-down.sh
+++ dev-down.sh
@@ -2,13 +2,13 @@
 
 direnv allow .
 
-tmux send-keys -t $SESSION:$CARDANO.0 C-c ;
-tmux send-keys -t $SESSION:$CARDANO.1 C-c ;
-tmux send-keys -t $SESSION:$CARDANO.2 C-c ;
+tmux send-keys -t "$SESSION":"$CARDANO".0 C-c ;
+tmux send-keys -t "$SESSION":"$CARDANO".1 C-c ;
+tmux send-keys -t "$SESSION":"$CARDANO".2 C-c ;
 
-tmux send-keys -t $SESSION:$APPS.0 C-c ;
-tmux send-keys -t $SESSION:$APPS.1 C-c ;
-tmux send-keys -t $SESSION:$APPS.2 C-c ;
-tmux send-keys -t $SESSION:$APPS.3 C-c ;
+tmux send-keys -t "$SESSION":"$APPS".0 C-c ;
+tmux send-keys -t "$SESSION":"$APPS".1 C-c ;
+tmux send-keys -t "$SESSION":"$APPS".2 C-c ;
+tmux send-keys -t "$SESSION":"$APPS".3 C-c ;
 
-tmux kill-session -t $SESSION
+tmux kill-session -t "$SESSION"
--- docker_build.sh
+++ docker_build.sh
@@ -2,8 +2,8 @@
 
 direnv allow .envrc
 
-docker build --progress=plain -t $GHCJS_IMAGE \
-    --build-arg uid=$USER_ID \
-    --build-arg username=$USER_NAME \
-    --build-arg docker_cabal_cache=$DOCKER_CABAL_CACHE \
-    --build-arg docker_front_path=$DOCKER_FRONT_PATH .
+docker build --progress=plain -t "$GHCJS_IMAGE" \
+    --build-arg uid="$USER_ID" \
+    --build-arg username="$USER_NAME" \
+    --build-arg docker_cabal_cache="$DOCKER_CABAL_CACHE" \
+    --build-arg docker_front_path="$DOCKER_FRONT_PATH" .
