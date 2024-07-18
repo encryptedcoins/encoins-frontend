45b9e13324f60e8293ff94419663ccdc4408e3c6
Vladislav Sabanov <email@email.com>
2024-07-18 10:12:57 +0500

    Remove duplicated env vars

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-USER_NAME=$(id -u -n)
-export USER_NAME
-USER_ID=$(id -u)
-export USER_ID
+# $USER, $UID and $HOME env vars taken from dockerfile wheree this script starts
+printf "\nConfiguring user: %s ...\n" "$USER"
 
-printf "\nConfiguring user: %s ...\n" "$USER_NAME"
-
-sudo adduser --disabled-password --gecos '' --uid "$USER_ID" "$USER_NAME" > /dev/null 2>&1 
-sudo adduser "$USER_NAME" sudo > /dev/null 2>&1 
+sudo adduser --disabled-password --gecos '' --uid "$UID" "$USER" > /dev/null 2>&1 
+sudo adduser "$USER" sudo > /dev/null 2>&1 
 
 if [ ! -d "$HOME"/.docker_cabal_cache ] || [ -z "$(ls -A "$HOME"/.docker_cabal_cache)" ]; then
     printf "\nFirst run. Preparing infrastructure..."
-    printf "\n\nUSER_NAME: %s" "$USER_NAME"
-    printf "\nUSER_ID: %s" "$USER_ID"
+    printf "\n\nUSER: %s" "$USER"
+    printf "\nUID: %s" "$UID"
     printf "\npwd: %s\n\n" "$(pwd)"
     cabal update
     cabal install alex-3.2.6
